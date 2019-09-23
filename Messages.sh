9c9a5c353dc6fde44927d788de06fcdd3851d89c
Yuri Chornoivan <yurchor@ukr.net>
2019-09-23 20:27:14 +0300

    Fix Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name 'libgit2*' -prune -o '(' -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' ')'` >> rc.cpp
-$XGETTEXT `find . -name 'libgit2*' -prune -o '(' -name '*.cpp' -o -name '*.h' -o -name '*.qml' ')'` -o $podir/kup.pot
+$EXTRACTRC `find . -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | grep -v 'libgit2'` >> rc.cpp
+$XGETTEXT `find . -name '*.cpp' -o -name '*.h' -o -name '*.qml' | grep -v 'libgit2'` -o $podir/kup.pot
 rm -f rc.cpp
