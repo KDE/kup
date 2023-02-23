KDE/kup

commit 6074bff29413f7f59b6e783487de8aea0ad6f874

Port away from deprecated KRun

daemon/planexecutor.cpp
@@ -12,10 +12,10 @@
 
 #include <KDiskFreeSpaceInfo>
 #include <KIO/DirectorySizeJob>
+#include <KIO/OpenUrlJob>
 #include <KFormat>
 #include <KLocalizedString>
 #include <KNotification>
-#include <KRun>
 #include <QDBusConnection>
 #include <QDBusReply>
 #include <QDir>
@@ -205,7 +205,8 @@ void PlanExecutor::notifyBackupSucceeded() {
 }
 
 void PlanExecutor::showLog() {
-	KRun::runUrl(QUrl::fromLocalFile(mLogFilePath), QStringLiteral("text/x-log"), nullptr, KRun::RunFlags());
+	auto *job = new KIO::OpenUrlJob(QUrl(mLogFilePath), QStringLiteral("text/x-log"));
+	job->start();
 }
 
 void PlanExecutor::startIntegrityCheck() {
@@ -438,7 +439,8 @@ void PlanExecutor::showBackupFiles() {
 		lArgs << mDestinationPath;
 		KProcess::startDetached(QStringLiteral("kup-filedigger"), lArgs);
 	} else if(mPlan->mBackupType == BackupPlan::RsyncType) {
-		KRun::runUrl(QUrl::fromLocalFile(mDestinationPath), QStringLiteral("inode/directory"), nullptr, KRun::RunFlags());
+		auto *job = new KIO::OpenUrlJob(QUrl::fromLocalFile(mDestinationPath));
+		job->start();
 	}
 }
 

CMakeLists.txt
@@ -5,7 +5,9 @@
 cmake_minimum_required(VERSION 3.0)
 cmake_policy(SET CMP0063 NEW)
 
-find_package(ECM REQUIRED NO_MODULE)
+set(KF_MIN_VERSION "5.71.0")
+
+find_package(ECM ${KF_MIN_VERSION} REQUIRED NO_MODULE)
 set(CMAKE_MODULE_PATH ${ECM_MODULE_PATH})
 
 project(kup)
@@ -23,7 +25,7 @@ include(ECMInstallIcons)
 include(ECMQtDeclareLoggingCategory)
 
 find_package(Qt5 REQUIRED COMPONENTS Widgets)
-find_package(KF5 REQUIRED COMPONENTS
+find_package(KF5 ${KF_MIN_VERSION} REQUIRED COMPONENTS
 Solid
 KIO
 IdleTime

daemon/planexecutor.h
@@ -12,7 +12,6 @@
 
 class KupDaemon;
 
-class KRun;
 class KNotification;
 class KProcess;
 

filedigger/filedigger.cpp
@@ -12,9 +12,10 @@
 #include <KFilePlacesView>
 #include <KFilePlacesModel>
 #include <KGuiItem>
+#include <KIO/OpenUrlJob>
+#include <KIO/JobUiDelegate>
 #include <KLocalizedString>
 #include <KMessageBox>
-#include <KRun>
 #include <KStandardAction>
 #include <KToolBar>
 
@@ -49,9 +50,12 @@ void FileDigger::updateVersionModel(const QModelIndex &pCurrent, const QModelInd
 }
 
 void FileDigger::open(const QModelIndex &pIndex) {
-	KRun::runUrl(pIndex.data(VersionBupUrlRole).toUrl(),
-	             pIndex.data(VersionMimeTypeRole).toString(), this, KRun::RunFlags());
 
+	auto *job = new KIO::OpenUrlJob(pIndex.data(VersionBupUrlRole).toUrl(),
+	             pIndex.data(VersionMimeTypeRole).toString());
+	auto *delegate = new KIO::JobUiDelegate(KIO::JobUiDelegate::AutoHandlingEnabled, this);
+	job->setUiDelegate(delegate);
+	job->start();
 }
 
 void FileDigger::restore(const QModelIndex &pIndex) {

filedigger/restoredialog.cpp
@@ -10,13 +10,14 @@
 #include "kupfiledigger_debug.h"
 
 #include <KIO/CopyJob>
+#include <KIO/OpenUrlJob>
+#include <KIO/JobUiDelegate>
 #include <KDiskFreeSpaceInfo>
 #include <KFileWidget>
 #include <KLocalizedString>
 #include <KMessageBox>
 #include <KMessageWidget>
 #include <KProcess>
-#include <KRun>
 #include <KWidgetJobTracker>
 
 #include <QDir>
@@ -392,10 +393,12 @@ void RestoreDialog::createNewFolder() {
 }
 
 void RestoreDialog::openDestinationFolder() {
-	KRun::runUrl(QUrl::fromLocalFile(mSourceInfo.mIsDirectory ?
+	auto *job = new KIO::OpenUrlJob(QUrl::fromLocalFile(mSourceInfo.mIsDirectory ?
 	                                    mFolderToCreate.absoluteFilePath() :
-	                                    mDestination.absolutePath()),
-	             QStringLiteral("inode/directory"), nullptr, KRun::RunFlags());
+	                                    mDestination.absolutePath()));
+	auto *delegate = new KIO::JobUiDelegate(KIO::JobUiDelegate::AutoHandlingEnabled, this);
+	job->setUiDelegate(delegate);
+	job->start();
 }
 
 void RestoreDialog::moveFolder() {

kcm/backupplanwidget.cpp
@@ -27,11 +27,11 @@
 #include <KComboBox>
 #include <KConfigDialogManager>
 #include <KConfigGroup>
+#include <KIO/OpenUrlJob>
 #include <KLineEdit>
 #include <KLocalizedString>
 #include <KMessageWidget>
 #include <KPageWidget>
-#include <KRun>
 #include <KUrlCompletion>
 #include <KUrlRequester>
 #include <utility>
@@ -872,10 +872,12 @@ KPageWidgetItem *BackupPlanWidget::createAdvancedPage(bool pPar2Available) {
 	lExcludesLabel->setTextFormat(Qt::RichText);
 	lExcludesLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
 	connect(lExcludesLabel, &QLabel::linkActivated, this, [this](QString pLink){
-		// call runUrl ourselves instead of QLabel and QDesktopService doing it, just
+		// open the URL ourselves instead of QLabel and QDesktopService doing it, just
 		// so that we can give a .html file ending to the temp file. Required for QWebEngine
 		// to understand that the file has html content. Firefox works either way.
-		KRun::runUrl(QUrl(pLink), QStringLiteral("text/html"), this, KRun::RunFlags(), QStringLiteral("manpage.html"));
+		auto *job = new KIO::OpenUrlJob(QUrl(pLink));
+		job->setSuggestedFileName("manpage.html");
+		job->start();
 	});
 	auto lLabelUpdater = [lExcludesLabel](bool pVersioned){
 		QString lHelpUrl = pVersioned ? QStringLiteral("man:///bup-index") : QStringLiteral("man:///rsync");

commit e9b5be857730a6ed1762efdec1a9d149375cc275

Fix log file not being opened
Had been broken by 6074bff2, but never made it into a release.

daemon/planexecutor.cpp
@@ -207,7 +207,7 @@ void PlanExecutor::notifyBackupSucceeded() {
 }
 
 void PlanExecutor::showLog() {
-	auto *job = new KIO::OpenUrlJob(QUrl(mLogFilePath), QStringLiteral("text/x-log"));
+	auto *job = new KIO::OpenUrlJob(QUrl::fromLocalFile(mLogFilePath), QStringLiteral("text/x-log"));
 	job->start();
 }
 