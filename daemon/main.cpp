fec627f4e900c8b390ff04db25ed51a6e7c5db01
Tobias Fella <tobias.fella@kde.org>
2026-03-31 15:29:14 +0200

    Ensure that notifications and jobs by the daemon have a nice title

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -47,6 +47,7 @@ int main(int argc, char *argv[])
                       i18n("© 2011–2020 Simon Persson"));
     lAbout.addAuthor(i18n("Simon Persson"), i18n("Maintainer"), "simon.persson@mykolab.com");
     lAbout.setTranslator(xi18nc("NAME OF TRANSLATORS", "Your names"), xi18nc("EMAIL OF TRANSLATORS", "Your emails"));
+    lAbout.setDesktopFileName("kup-daemon");
     KAboutData::setApplicationData(lAbout);
 
     KCrash::initialize();
